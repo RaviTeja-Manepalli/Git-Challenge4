bdb9ad1695efbf564a87c8a28416b72b795f0f01
RaviTeja-Manepalli <ravitejamanepalli47@gmail.com>
2021-02-05 18:30:43 +0530

    First Commit

---
+++ GitMergeConflict/src/Caculation.java
@@ -0,0 +1,12 @@
+
+public class Caculation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=7;
+		System.out.println("Addition "+ a+b);
+
+	}
+
+}
