a39b1fcbb2a1ded390f8d2ca9a685d2eedf8c83c
RaviTeja-Manepalli <ravitejamanepalli47@gmail.com>
2021-02-06 16:55:03 +0530

    logic changed

--- GitMergeConflict/src/Caculation.java
+++ GitMergeConflict/src/Caculation.java
@@ -5,8 +5,8 @@ public class Caculation {
 		// TODO Auto-generated method stub
 		int z=5;
 		int x=7;
-		int t=2;
-		System.out.println("Multiply "+ (z*t*x));
+		int a=2;
+		System.out.println("Multiply "+ (z*a*x));
 
 	}
 
