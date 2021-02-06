b7863cdb17703d99d4200a2aab508ce199ac3d7d
RaviTeja-Manepalli <ravitejamanepalli47@gmail.com>
2021-02-06 17:20:09 +0530

    Changedddd

--- GitMergeConflict/src/Caculation.java
+++ GitMergeConflict/src/Caculation.java
@@ -4,8 +4,8 @@ public class Caculation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int x=7;
-		int z=2;
-		System.out.println("Multiply "+ (z*+x*z+x));
+		int a=2;
+		System.out.println("Multiply "+ (a*+x*a+x));
 
 	}
 
