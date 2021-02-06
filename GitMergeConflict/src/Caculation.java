ac804f070d35a751f06f7f4f852e635408c01a61
RaviTeja-Manepalli <ravitejamanepalli47@gmail.com>
2021-02-06 17:34:16 +0530

    Changed branch1

--- GitMergeConflict/src/Caculation.java
+++ GitMergeConflict/src/Caculation.java
@@ -5,7 +5,9 @@ public class Caculation {
 		// TODO Auto-generated method stub
 		int f=7;
 		int d=2;
-		System.out.println("Multiply "+ (d*f*f+d));
+		int g=89;
+		System.out.println("Multiply "+ (d*d*f+d));
+		System.out.println("addition" + (f+d+g));
 
 	}
 
