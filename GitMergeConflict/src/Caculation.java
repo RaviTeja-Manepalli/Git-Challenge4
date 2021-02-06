58e31d94c6d1e5c8a541adad94f8929eb35b6a1c
RaviTeja-Manepalli <ravitejamanepalli47@gmail.com>
2021-02-06 17:50:47 +0530

    committed feature

--- GitMergeConflict/src/Caculation.java
+++ GitMergeConflict/src/Caculation.java
@@ -4,10 +4,10 @@ public class Caculation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int f=7;
-		int d=2;
+		int r=2;
 		int g=89;
-		System.out.println("Multiply "+ (d*d*f+d));
-		System.out.println("addition" + (f+d+g));
+		System.out.println("Multiply "+ (r*r*f+r));
+		System.out.println("addition" + (f+r+g));
 
 	}
 
