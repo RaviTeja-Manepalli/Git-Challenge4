a4f3237e58dc39908ec60cd72f6bf661f0217a0c
RaviTeja-Manepalli <ravitejamanepalli47@gmail.com>
2021-02-06 17:12:44 +0530

    changed the logiccc

--- GitMergeConflict/src/Caculation.java
+++ GitMergeConflict/src/Caculation.java
@@ -4,8 +4,8 @@ public class Caculation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int x=7;
-		int y=2;
-		System.out.println("Multiply "+ (y*y+x*y+x));
+		int z=2;
+		System.out.println("Multiply "+ (z*+x*z+x));
 
 	}
 
