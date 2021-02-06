de8c4133b4d58788a4de39a434d201d6b9a6879c
RaviTeja <ravitejamanepalli47@gmail.com>
2021-02-06 16:58:21 +0530

    Update Caculation.java

--- GitMergeConflict/src/Caculation.java
+++ GitMergeConflict/src/Caculation.java
@@ -3,10 +3,9 @@ public class Caculation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int z=5;
 		int x=7;
 		int y=2;
-		System.out.println("Multiply "+ (z+y*x));
+		System.out.println("Multiply "+ (y*y*x));
 
 	}
 
