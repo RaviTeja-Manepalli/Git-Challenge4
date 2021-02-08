6beaa3d763a0345e63f3e1e8070ba03be132e16a
RaviTeja <ravitejamanepalli47@gmail.com>
2021-02-08 17:18:06 +0530

    Update Add.java

--- GitMergeConflict/src/Add.java
+++ GitMergeConflict/src/Add.java
@@ -3,9 +3,9 @@ public class Add {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=5;
+		int e=5;
 		int c= 7;
-		System.out.println(a+c);
+		System.out.println(e*c);
 
 	}
 
